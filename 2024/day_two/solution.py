67df705b39205edb8fb789422a6d6a6ece69c8ed
MartinPettersen <searchingtheskyfor@gmail.com>
2024-12-02 12:58:37 +0100

    solution day 2

--- 2024/day_two/solution.py
+++ 2024/day_two/solution.py
@@ -1,19 +1,28 @@
 import numpy as np
 
-def check_number_range(number, list):
+def check_number_range(list):
+    
+    isSafe = True
     
     for num in list:
         if abs(num) < 1:
-            return False
+            isSafe = False
         if abs(num) > 3:
-            return False
-    return True
+            isSafe = False
+    return isSafe
+
 
 def check_for_unsafe_dif(list):
     difference = np.diff(list)
-    res = check_number_range(3, difference)
+    res = check_number_range(difference)
     return res
-    #print(difference)
+
+
+def increase_decrease_bad_lvl_check(list):
+    temp_num = None
+    for num in list:
+        if temp_num == None:
+            temp_num = num
 
 def check_if_always_decrease(list):
     list_copy = list.copy()
@@ -39,23 +48,44 @@ def get_lists(file_name):
             res = map(int, temp_line)
             lists.append(list(res))
     return lists
+   
+def checks(list):
+    res_increase = check_if_always_increase(list)  
+    res_decrease = check_if_always_decrease(list)  
+        
+    if ( res_increase or res_decrease):
+        res = check_for_unsafe_dif(list)
+        if res:
+            return True
+    return False
+        
+def bad_level_dampener(list):
+    
+    bad_level_saved = False
+    
+    for i in range(len(list)):
+        list_copy = list.copy()
+        list_copy.pop(i)
+        res = checks(list_copy)
+        if res:
+            bad_level_saved = True
+    return bad_level_saved
     
 def check_for_change(lists):
     
     safe_counter = 0
     
     for list in lists:
-        res_increase = check_if_always_increase(list)  
-        #print(res_increase)
-        res_decrease = check_if_always_decrease(list)  
-        #print(res_decrease)
-        
-        if ( res_increase or res_decrease):
-            res = check_for_unsafe_dif(list)
-            if res:
+        res = checks(list)
+        if res:
+            safe_counter += 1
+        else:
+            saved = bad_level_dampener(list)
+            if saved:
                 safe_counter += 1
+                
+                    
     print("safe: ",safe_counter)
         
 lists = get_lists("input.txt")
-#print(lists)
 check_for_change(lists)
