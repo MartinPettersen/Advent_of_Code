08bf7f2483c9328e19f07d31e60c0c6250f4db42
MartinPettersen <searchingtheskyfor@gmail.com>
2024-12-04 12:52:51 +0100

    add solution day 4 part 2

--- 2024/day_four/solution.py
+++ 2024/day_four/solution.py
@@ -1,128 +1,59 @@
 import re
 from GetData import get_file_data
 
-def check_down(data, index, i, left, right):
-    match_count = 0
+def detect_X_mas(data,index, i):
+    isPossible = True
     
-    down_text = ""
-    for j in range(4):
-        down_text += (data[i + j][index])
-    if (down_text == "XMAS"):
-        match_count += 1
+    text_down_right = ""
+    text_up_right = ""
     
-    if left:
-        left_text = ""
-        for j in range(4):
-            left_text += (data[i + j][index-j])
-        if (left_text == "XMAS"):
-            match_count += 1
-    
-    if right:
-        right_text = ""
-        for j in range(4):
-            right_text += (data[i + j][index+j])
-        print(right_text)
-        if (right_text == "XMAS"):
-            match_count += 1
-    
-    return match_count
+    try:
+        if index > 0 and i > 0:
+            text_down_right += data[i - 1][index - 1] + "A"
+        else:
+            isPossible = False
+    except:
+        print("someting wrong top-left")
+    try:
+        if (len(data) > (i+1)) and i > 0:
+            text_up_right += data[i + 1][index - 1] + "A"
+        else:
+            isPossible = False
+    except:
+        print("someting wrong bottom-left")
+    try:
+        if index > 0 and (len(data[i]) > (index + 1)):
+            text_up_right += data[i - 1][index + 1]
+        else:
+            isPossible = False
+    except:        
+        print("someting wrong top-right")
+    try:
+        if (len(data) > (i+1))  and (len(data[i]) > (index+1)):
+            text_down_right += data[i + 1][index + 1]
+        else:
+            isPossible = False
+    except:
+        print("someting wrong bottom-right")
+    if isPossible:
+        if (text_down_right == "MAS" or text_down_right == "SAM") and (text_up_right == "MAS" or text_up_right == "SAM"):
+            return 1
+    return 0
 
+def detect_a(line):
+    a_matches = re.finditer('A',line)
+    a_indexes = []
+    for match in a_matches:
+        a_indexes.append(match.span()[0])
+    return a_indexes
 
-def check_up(data, index, i, left, right):
-    match_count = 0
-    
-    up_text = ""
-    for j in range(4):
-        up_text += (data[i - j][index])
-    if (up_text == "XMAS"):
-        match_count += 1
-    
-    if left:
-        left_text = ""
-        for j in range(4):
-            left_text += (data[i - j][index-j])
-        print(left_text)
-        if (left_text == "XMAS"):
-            match_count += 1
-    
-    if right:
-        right_text = ""
-        for j in range(4):
-            right_text += (data[i - j][index+j])
-        if (right_text == "XMAS"):
-            match_count += 1
-    
-    return match_count
-
-def check_horizontal(data, index, i, left, right):
-    
-    match_count = 0
-    
-    if right:
-        text = data[i][index:(4 + index)]
-        if (text == "XMAS"):
-            match_count += 1
-
-    if left:    
-        text = data[i][(-4 + (index+1)):(index+1)]
-        if (text == "SAMX"):
-            match_count += 1
-
-    return match_count
-
-def candidate_check(data,index, i):
-    left_posibility = False
-    right_posibility = False
-    up_posibility = False
-    down_posibility = False
-
-    total_match_count = 0
-    
-    print("len", len(data[i]), "i",i )
-
-    if (len(data[i]) - index) > 3:
-        right_posibility = True 
-    
-    if (index) >= 3:
-        left_posibility = True 
-
-    if i > 2:
-        up_posibility = True    
-        
-    if (len( data) - (len("XMAS")+ i)) >= 0:
-        down_posibility = True
-    
-    print("left_posibility",left_posibility)
-    print("right_posibility",right_posibility)
-    print("up_posibility",up_posibility)
-    print("down_posibility",down_posibility)
-    if (left_posibility or right_posibility):
-        total_match_count += check_horizontal(data, index, i, left_posibility, right_posibility)
-    
-    if up_posibility:
-        total_match_count += check_up(data, index, i, left_posibility, right_posibility)
-    
-    if down_posibility:
-        total_match_count += check_down(data, index, i, left_posibility, right_posibility)
-    
-        
-    return total_match_count
-    
-def detect_x(line):
-    x_matches = re.finditer('X',line)
-    x_indexes = []
-    for match in x_matches:
-        x_indexes.append(match.span()[0])
-    return x_indexes
-
-def main():
-    data = get_file_data("input.txt")
+def main(file_name):
+    data = get_file_data(file_name)
     total_match_count = 0
     for i in range(len(data)):
-        x_indexes = detect_x(data[i])
-        print(x_indexes)
-        for index in x_indexes:
-            total_match_count += candidate_check(data, index, i)
+        a_indexes = detect_a(data[i])
+        for index in a_indexes:
+            total_match_count += detect_X_mas(data, index, i)
     print("total mathces: ", total_match_count)
     
-main()
+main("input.txt")
